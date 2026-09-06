a159968c76ea33054575e240e7efabf6f379c93c
vivekpunde05 <vivekpunde6@gmail.com>
2026-09-06 22:52:09 +0530

    first commit

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,6 @@ import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
