b653d2cdd25b673cae039da9dd45588b1b3d9a24
vivekpunde05 <vivekpunde6@gmail.com>
2026-09-11 20:10:07 +0530

    new

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-
 from sklearn.model_selection import train_test_split
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
