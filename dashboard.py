815abc0e813a5591731ecc53a2323b6c0e242e24
vivekpunde05 <vivekpunde6@gmail.com>
2026-09-14 11:12:34 +0530

    new

--- dashboard.py
+++ dashboard.py
@@ -10,6 +10,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
+
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import 
