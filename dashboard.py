97ca28603cbadcaa3a0202cb4ff6f1c35eca610d
vivekpunde05 <vivekpunde6@gmail.com>
2026-09-14 11:12:20 +0530

    new

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import (
+from sklearn.metrics import 
+(
     accuracy_score, precision_score, recall_score, f1_score,
     confusion_matrix, roc_auc_score
     )
