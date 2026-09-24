50eea154ce708759860df82fe1415bf88b8de21d
vivekpunde05 <vivekpunde6@gmail.com>
2026-09-24 21:29:56 +0530

    new

--- dashboard.py
+++ dashboard.py
@@ -16,6 +16,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import (
     accuracy_score, precision_score, recall_score, f1_score,
     confusion_matrix, roc_auc_score
+    
 )
 
 # PAGE CONFIG
